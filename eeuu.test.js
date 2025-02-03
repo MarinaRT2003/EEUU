2738d0472868f27920570e44a971962f3586b98c
MarinaRT2003 <mreptru297@g.educaand.es>
2025-02-03 12:55:57 +0100

    as2

--- eeuu.test.js
+++ eeuu.test.js
@@ -1,9 +1,9 @@
 const {validateTarget} = require('./eeuu');
 
 test ('validateTarget', () => {
-    expect(validateTarget(53, 135)).toBe(true);
+    expect(validateTarget(53, 135))(true);
 });
 
 test ('validateTarget', () => {
-    expect(validateTarget(53, 136)).toBe(false);
+    expect(validateTarget(53, 136))(false);
 });
